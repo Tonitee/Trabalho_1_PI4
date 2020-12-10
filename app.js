686b69f15ec43fbfc5be16016850e7626d78142a
Tonitee <toni99.st@gmail.com>
2020-12-09 21:34:49 -0300

    Trabalho Final

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const port = 3000
+
+//Importa Rotas
+const rotaProduto = require('./rotas/produto_rota');
+const rotaUsuario = require('./rotas/usuario_rota');
+
+//Importa usuario_controller para verificar token
+const controller = require('./controller/usuario_controller')
+
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+//Configuração do Mongoose
+mongoose.connect('mongodb://localhost/app_produtos', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }).then(()=> {
+    console.log('BD conectado');
+  })
+  .catch((error)=> {
+    console.log('Erro ao conectar ao BD');
+  });
+mongoose.Promise = global.Promise;
+
+//Middleware para Log
+app.use((req,resp,next) => {
+  console.log("Request Time: "+Date.now());
+  console.log("Method: "+req.method)
+  next();
+});
+
+
+
+//Uso das rotas
+app.use('/api/produtos', controller.validaToken, rotaProduto);
+
+app.use('/api/usuarios', rotaUsuario);
+
+app.listen(port, () => {
+  console.log(`Iniciando o servidor: http://localhost:${port}`)
+})
---
+++ controller/usuario_controller.js
@@ -0,0 +1,109 @@
+const Usuario = require('../model/usuario')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+
+exports.listar = (req, res) => { 
+    Usuario.find({},(err, usuarios) => {
+        if(err){
+            res.status(500).send(err);
+        }
+        res.json(usuarios);
+    });
+}
+
+
+exports.inserir = (req, res) => {
+    let novoUsuario = new Usuario(req.body);
+    novoUsuario.senha = bcrypt.hashSync(req.body.senha,10);        
+    novoUsuario.save((err, usuario) => {
+        if(err){
+            res.send(err);
+        }    
+        res.status(201).json(usuario);
+        
+    });
+}
+
+exports.atualizar = (req, res) => {
+    let id = req.params.id;
+    let usuarioAtualizar = req.body;
+    Usuario.findOneAndUpdate({ _id: id }, usuarioAtualizar, { new: true }, (err, usuarioAtual) => {
+        if(err){
+            res.send(err);
+        }
+        res.json(usuarioAtual);
+    });
+}
+
+exports.deletar = (req, res) => {
+    let id = req.params.id;
+    Usuario.findOneAndDelete({ _id: id }, (err, usuarioAtual) => {
+        if(err){
+            res.send(err);
+        }
+        res.json(usuarioAtual);
+    });
+}
+
+exports.buscarPorId = (req, res) => {
+    let id = req.params.id;
+    Usuario.findById(id, (err, usuario) => {
+        if(err)
+            res.status(500).send(err);        
+        res.json(usuario);
+    });    
+}
+
+exports.buscarUsuario = (req, res, next) => {
+    if (req.query && req.query.usuario){
+        const paramUsuario = req.query.usuario;
+        console.log(paramUsuario);
+        Usuario.findOne({usuario: paramUsuario}, (err, usuario) => {
+            if(err){
+                res.status(500).send(err);
+            }
+            res.json(usuario);
+        });
+    }    
+}
+
+exports.validaUsuario = (req, res, next) => {
+    if (req.body && req.body.usuario && req.body.senha){
+        const username = req.body.usuario;
+        const senha = req.body.senha
+        Usuario.findOne({usuario: username}, (err, usuario) => {
+            if(err){
+                res.status(500).send(err);
+            }
+            const valido = bcrypt.compareSync(senha, usuario.senha);
+            if(usuario && valido){
+                const token = jwt.sign({
+                    id: usuario.id
+                }, 'Sen@cr5', {expiresIn: "1h"});
+                res.status(201).send({"token":token});
+            }
+            else{
+                res.status(401).send("Usuário ou senha inválidos");
+            }
+        });
+    }
+}
+
+exports.validaToken = (req, res, next) => {
+    const token = req.get("x-auth-token");
+    if(!token) {
+        res.status(401).send("Não possui o token de acesso");
+    }
+    else {
+        jwt.verify(token,'Sen@cr5',(err,userId) =>{
+            if(err){
+                res.status(401).send(err);
+            }
+            else {
+                console.log("Usuário autorizado: "+userId);
+                next();
+            }
+        })
+    }
+}
